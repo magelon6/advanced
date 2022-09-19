1c5d76768d5f5f12f15b35b60f72e4e784fac127
magelon6 <adizovmsu@gmail.com>
2022-09-19 12:41:23 +0300

    feat: add 2 themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React, {Suspense} from 'react';
-import Counter from "./Components/Counter";
-import './index.scss'
+import React, {Suspense, useContext, useState} from 'react';
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import AboutPage from "./Pages/AboutPage/AboutPage";
-import MainPage from './Pages/MainPage/MainPage';
 import {AboutPageAsync} from "./Pages/AboutPage/AboutPageAsync";
 import {MainPageAsync} from "./Pages/MainPage/MainPageAsync";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+
+    const{theme, toggleTheme} = useTheme()
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
