3303080eb25bd0bd61835d7500769322c3e12454
magelon6 <adizovmsu@gmail.com>
2022-09-19 13:41:01 +0300

    classnames theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./Pages/AboutPage/AboutPageAsync";
 import {MainPageAsync} from "./Pages/MainPage/MainPageAsync";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
 
     const{theme, toggleTheme} = useTheme()
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
