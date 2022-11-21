ffc860094499930d30e7e1a892ab8ccb3f2f92f2
magelon6 <adizovmsu@gmail.com>
2022-11-21 13:20:57 +0300

    feat: screenshot tests and CI

--- src/app/providers/ErrorBoundary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -1,4 +1,4 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+// import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
