eefaab6d59d7199cf2160755d0f22c6f0bcaede6
magelon6 <adizovmsu@gmail.com>
2022-11-27 20:55:31 +0300

    fix: test fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,7 @@ import { Sidebar } from 'widgets/Sidebar';
 
 function App() {
     const { theme } = useTheme();
-
+    // wda
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
